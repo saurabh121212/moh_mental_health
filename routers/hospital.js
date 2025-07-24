e39e5627b0988c1e8d2497955bd00a1e644a613e
Saurabh <saurabhsaini38@gmail.com>
2025-07-24 23:14:55 +0530

    update

--- routers/hospital.js
+++ routers/hospital.js
@@ -136,6 +136,9 @@ router.get('/hospital-all-upcoming-appointments/:id', authMiddleware.authHospita
 router.get('/hospital-all-past-appointments/:id', authMiddleware.authHospital, HospitalController.hospitalAllPastAppointments);
 
 // This is used to get the details of a Assessment Test
-router.get('/get-assessment-test-single-detail/:id', authMiddleware.authAdmin, HospitalController.getAssessmentTestDetailsSingle);
+router.get('/get-assessment-test-single-detail/:id', authMiddleware.authHospital, HospitalController.getAssessmentTestDetailsSingle);
+
+// This is same as the above one but this is used in Admin panel
+router.get('/get-assessment-test-single-detail-v2/:id', authMiddleware.authAdmin, HospitalController.getAssessmentTestDetailsSingle);
 
 module.exports = router;
