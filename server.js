b0e63ab1eec899edb278376972c25045364bc52b
Saurabh <saurabhsaini38@gmail.com>
2025-08-06 13:17:43 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
+const startCronJobs = require('./crons/index.js');
+//startCronJobs();
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
